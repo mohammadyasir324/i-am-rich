689e18d1ccb4babc4a49468999b04b6421e00f0c
mohammadyasir324 <mohammadyasirmirzada1111@gmail.com>
2024-09-29 12:15:21 +0430

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://example.com/path_to_rich_image.png'),  // Add an image URL here.
+          ),
+        ),
+      ),
+    );
+  }
+}
